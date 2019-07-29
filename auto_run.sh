08edd941445dce62e2d60fd148898fa4fe259355
Shuai <liustark@hotmail.com>
2019-07-29 20:56:08 +0800

    auto run ok

--- auto_run.sh
+++ auto_run.sh
@@ -4,15 +4,16 @@ ps_num=1
 worker_num=2
 
 
-ps_num -= 1
-worker_num -= 1
+#let "ps_num -= 1"
+#let "worker_num -= 1"
 
-for i in `eval echo {0..$ps_num}`
+
+for i in `eval echo {0..$((ps_num-1))}`
 do
   CUDA_VISIBLE_DEVICES='' python ddpg.py --job_name="ps" --workers_num $worker_num --task_index=$i &
 done
 
-for i in `eval echo {0..$worker_num}`
+for i in `eval echo {0..$(($worker_num-1))}`
 do
   CUDA_VISIBLE_DEVICES='' python ddpg.py --job_name="worker" --workers_num $worker_num --task_index=$i &
 done
